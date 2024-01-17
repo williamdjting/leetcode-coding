cc0b318285d9657c0eb01c7bbd270c53eb4f0d16
William Ting <williamdjting@gmail.com>
2024-01-16 19:42:17 -0800

    add

---
+++ python/Remove-duplicates-from-sorted-array.py
@@ -0,0 +1,16 @@
+  # Remove Duplicates from Sorted Array
+
+nums = [1,1,2]
+
+def removeDuplicates(nums):
+    num1 = 0
+    for i in range(nums):
+      if nums[num1] == nums[i+1]:
+        nums[i+1] == nums[i+2]
+      
+
+      
+
+
+
+
