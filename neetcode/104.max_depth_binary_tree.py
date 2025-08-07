a4a0347475edd18185c66de5dcec33b5b5847630
William Ting <williamting@Williams-Mac-mini.local>
2025-08-06 22:53:48 -0700

    104

---
+++ neetcode/104.max_depth_binary_tree.py
@@ -0,0 +1,34 @@
+from typing import Optional
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        
+
+        if root is None:
+            return 0
+        
+
+
+        count = 1 + max( self.maxDepth(root.left), self.maxDepth(root.right) )
+
+
+        print("count", count)
+        return count
+    
+# root = [3,9,20,None,None,15,7]
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+sol = Solution()
+
+sol.maxDepth(root)
