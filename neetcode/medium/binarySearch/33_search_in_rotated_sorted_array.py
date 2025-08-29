71a236cee81811daf86ace38a89b9baa3df6ae96
William Ting <williamting@Williams-Mac-mini.local>
2025-08-29 00:25:10 -0700

    add 33

---
+++ neetcode/medium/binarySearch/33_search_in_rotated_sorted_array.py
@@ -0,0 +1,22 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+
+
+
+
+
+
+
+        return -1
+
+
+
+
+nums = [4,5,6,7,0,1,2]
+target = 0
+
+sol = Solution()
+
+answer = sol.search()
+
+print("this is answer", answer)
